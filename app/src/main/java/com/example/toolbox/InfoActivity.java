792c4bfd8ea7c9f46baa34a06df0e3cbf54472a9
kdesai0512 <desai8252@students.d211.org>
2019-09-13 11:57:19 -0500

    Finished subjects. End of class Fri.

--- app/src/main/java/com/example/toolbox/InfoActivity.java
+++ app/src/main/java/com/example/toolbox/InfoActivity.java
@@ -3,13 +3,21 @@ package com.example.toolbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class InfoActivity extends AppCompatActivity {
+
+    public static final String MY_NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+
+    public void checked(View v) {
+
     }
 
 
