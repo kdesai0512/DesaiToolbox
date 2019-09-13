aae94f860da9b430d36f87004545341a5a8cdf93
kdesai0512 <desai8252@students.d211.org>
2019-09-12 22:20:39 -0500

    More check boxes. End of Thurs.

--- app/src/main/java/com/example/toolbox/InfoActivity.java
+++ app/src/main/java/com/example/toolbox/InfoActivity.java
@@ -11,4 +11,6 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
     }
+
+
 }
--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.toolbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void begin(View v) {
 
+        EditText myName = (EditText) findViewById(R.id.name);
+
+        if (myName.length() == 0) {
+            Context context = getApplicationContext();
+            CharSequence text = "Please fill all blanks";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            String myNameStr = myName.getText().toString();
+
+
+            Intent intent = new Intent(this, InfoActivity.class);
+
+            intent.putExtra(InfoActivity.MY_NAME, myNameStr);
+
+
+            startActivity(intent);
+        }
     }
+
+
 }
