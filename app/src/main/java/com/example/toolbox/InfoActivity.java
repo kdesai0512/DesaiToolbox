6e4717e8d1cf9a601bc09e5574e8e8df4b2f60a5
kdesai0512 <desai8252@students.d211.org>
2019-09-16 11:58:05 -0500

    Finished InfoActivity. End of Mon.

--- app/src/main/java/com/example/toolbox/InfoActivity.java
+++ app/src/main/java/com/example/toolbox/InfoActivity.java
@@ -17,7 +17,41 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     public void checked(View v) {
+        CheckBox s = (CheckBox) findViewById(R.id.science);
+        CheckBox m = (CheckBox) findViewById(R.id.math);
+        CheckBox h = (CheckBox) findViewById(R.id.history);
+        CheckBox e = (CheckBox) findViewById(R.id.english);
+        CheckBox a = (CheckBox) findViewById(R.id.art);
+        CheckBox u = (CheckBox) findViewById(R.id.music);
+        CheckBox c = (CheckBox) findViewById(R.id.comp);
 
+        if(s.isChecked()) {
+            String science = "science homework";
+        }
+
+        if(m.isChecked()) {
+            String math = "math homework";
+        }
+
+        if(h.isChecked()) {
+            String history = "history homework";
+        }
+
+        if(e.isChecked()) {
+            String english = "english homework";
+        }
+
+        if(a.isChecked()) {
+            String art = "art homework";
+        }
+
+        if(u.isChecked()) {
+            String music = "music homework";
+        }
+
+        if(c.isChecked()) {
+            String comp = "computer science homework";
+        }
     }
 
 
