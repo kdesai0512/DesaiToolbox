dbdb713af2b0c8ccf82d8b08e11d083bfb0e13cc
kdesai0512 <desai8252@students.d211.org>
2019-09-17 23:32:37 -0500

    Finished textboxes. Need to finish Info2Activity. End of Tues.

--- app/src/main/java/com/example/toolbox/Info2Activity.java
+++ app/src/main/java/com/example/toolbox/Info2Activity.java
@@ -3,12 +3,37 @@ package com.example.toolbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Info2Activity extends AppCompatActivity {
+
+    public static final String MY_SCIENCE = "";
+    public static final String MY_MATH = "";
+    public static final String MY_HISTORY = "";
+    public static final String MY_ENGLISH = "";
+    public static final String MY_ART = "";
+    public static final String MY_MUSIC = "";
+    public static final String MY_COMP = "";
+    private String message = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info2);
     }
+
+    public void set(View v) {
+        if (MY_SCIENCE.length() != 0)
+            message += MY_SCIENCE + "\n";
+        if (MY_MATH.length() != 0)
+            message += MY_MATH + "\n";
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/toolbox/InfoActivity.java
+++ app/src/main/java/com/example/toolbox/InfoActivity.java
@@ -2,13 +2,25 @@ package com.example.toolbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InfoActivity extends AppCompatActivity {
 
     public static final String MY_NAME = "name";
+    private String science;
+    private String math;
+    private String history;
+    private String english;
+    private String art;
+    private String music;
+    private String comp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,43 +28,90 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
     }
 
-    public void checked(View v) {
-        CheckBox s = (CheckBox) findViewById(R.id.science);
-        CheckBox m = (CheckBox) findViewById(R.id.math);
-        CheckBox h = (CheckBox) findViewById(R.id.history);
-        CheckBox e = (CheckBox) findViewById(R.id.english);
-        CheckBox a = (CheckBox) findViewById(R.id.art);
-        CheckBox u = (CheckBox) findViewById(R.id.music);
-        CheckBox c = (CheckBox) findViewById(R.id.comp);
-
-        if(s.isChecked()) {
-            String science = "science homework";
-        }
-
-        if(m.isChecked()) {
-            String math = "math homework";
-        }
-
-        if(h.isChecked()) {
-            String history = "history homework";
-        }
-
-        if(e.isChecked()) {
-            String english = "english homework";
-        }
-
-        if(a.isChecked()) {
-            String art = "art homework";
-        }
-
-        if(u.isChecked()) {
-            String music = "music homework";
-        }
-
-        if(c.isChecked()) {
-            String comp = "computer science homework";
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.science:
+                if (checked)
+                    science = "science homework";
         }
     }
+
+    public void onCheckbox2Clicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.math:
+                if (checked)
+                    math = "math homework";
+        }
+    }
+
+    public void onCheckbox3Clicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.history:
+                if (checked)
+                    history = "history homework";
+        }
+    }
+
+    public void onCheckbox4Clicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.english:
+                if (checked)
+                    english = "english homework";
+        }
+    }
+
+    public void onCheckbox5Clicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.art:
+                if (checked)
+                    art = "art homework";
+        }
+    }
+
+    public void onCheckbox6Clicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.music:
+                if (checked)
+                    music = "music homework";
+        }
+    }
+
+    public void onCheckbox7Clicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.comp:
+                if (checked)
+                    comp = "computer science homework";
+        }
+    }
+
+    public void generate(View v) {
+
+
+        Intent intent = new Intent(this, Info2Activity.class);
+
+        intent.putExtra(Info2Activity.MY_SCIENCE, science);
+        intent.putExtra(Info2Activity.MY_MATH, math);
+        intent.putExtra(Info2Activity.MY_HISTORY, history);
+        intent.putExtra(Info2Activity.MY_ENGLISH, english);
+        intent.putExtra(Info2Activity.MY_ART, art);
+        intent.putExtra(Info2Activity.MY_MUSIC, music);
+        intent.putExtra(Info2Activity.MY_COMP, comp);
+
+        startActivity(intent);
+
+    }
+
+
+
+
+
 
 
 }
